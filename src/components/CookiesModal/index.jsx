14541392b8635f1376fb87b85c7dd98d54db9801
josuesan <josueguitar2@gmail.com>
2021-04-05 18:15:09 -0400

    styles: add modal styles

--- src/components/CookiesModal/index.jsx
+++ src/components/CookiesModal/index.jsx
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { useContext, useState } from 'react';
 import {
-  Button,
   Col,
   Modal,
-  Row,
+  Row
 } from 'react-bootstrap';
 import { useCookies } from 'react-cookie';
 import { CookiesContext } from '../../contexts/CookiesContext';
@@ -25,27 +24,25 @@ const CookiesModal = ({ initAnalitics }) => {
   };
 
   return (
-    <>
-      <Modal centered size="lg" show={show} onHide={handleAcceptCookies} className="modal-cookies-form">
-        <Modal.Body className="px-3 px-md-5 pb-5 pt-4">
-          <p className="bold p-lg">Política de Cookies</p>
-          <p>
-            Te informamos que a través de este sitio web utilizamos cookies para mejorar la experiencia de usuario.
-            Asumimos que al seguir navegando aceptas su uso.
+    <Modal centered size="md" show={show} onHide={handleAcceptCookies} className="modal-cookies-form">
+      <Modal.Body className="p-3">
+        <p className="bold p-lg">Política de Cookies</p>
+        <p className="mb-3">
+          Te informamos que a través de este sitio web utilizamos cookies para mejorar la experiencia de usuario.
+          Asumimos que al seguir navegando aceptas su uso.
           </p>
-          <Row className="justify-content-end flex-1 align-content-end">
-            <Col xs="12" md>
-              <Button
-                className="rounded-pill p-sm py-2 d-block w-100 das-btn mx-auto mr-md-0 ml-md-auto mt-3 mt-md-0"
-                onClick={handleAcceptCookies}
-              >
-                <span>OK</span>
-              </Button>
-            </Col>
-          </Row>
-        </Modal.Body>
-      </Modal>
-    </>
+        <Row className="justify-content-end flex-1 align-content-end mb-3">
+          <Col xs="12">
+            <button
+              className="btn btn-2 text-accent-light hoverable mx-auto bold d-block"
+              onClick={handleAcceptCookies}
+            >
+              OK
+              </button>
+          </Col>
+        </Row>
+      </Modal.Body>
+    </Modal>
   );
 };
 
